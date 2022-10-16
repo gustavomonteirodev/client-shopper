73fef22ef618c2a68bd3efe84c4a7f53fe3961af
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-10-16 19:23:46 -0300

    bugs do estoque resolvido

--- src/pages/StockPage/StockPage.js
+++ src/pages/StockPage/StockPage.js
@@ -3,13 +3,10 @@ import { Loader } from "../../pages/HomePage/HomePageStyles";
 import StockCard from '../../components/StockCard/StockCard'
 import { BASE_URL } from "../../constants/url"
 import { useRequestData } from '../../hooks/useRequestData'
-import { useNavigate } from 'react-router-dom'
-import Button from '@mui/material/Button'
 import { ButtonAndTitle } from './StockPageStyles'
-import { goToHome } from "../../routes/coordinator"
 
 const  StockPage = () => {
-  const navigate = useNavigate()
+
   const { data } = useRequestData(`${BASE_URL}/products`, undefined)
    
   const productsList = data && data.map((product) => {
